3844c3f15403c4ab9ee1dc79720efd14ceb1eb9a
Murilo Trevejo <murilotrevejo09@gmail.com>
2025-04-02 22:18:50 -0300

    fix images

--- src/mocks/products.ts
+++ src/mocks/products.ts
@@ -7,7 +7,7 @@ export const testProducts: Product[] = [
     "name": "Espresso",
     "description": "Strong concentrated coffee served in small shots",
     "price": 3.99,
-    "image_url": "https://images.pexels.com/photos/1695052/pexels-photo-1695052.jpeg"
+    "image_url": "https://img.freepik.com/free-photo/closeup-classic-fresh-espresso-served-dark-surface_1220-5376.jpg?t=st=1743639229~exp=1743642829~hmac=c04bb8cb9c155d26de7afda689d7c9bd53283fff236c3a5a06489b5f780e4886&w=740"
   },
   {
     "id": 2,
@@ -15,7 +15,7 @@ export const testProducts: Product[] = [
     "name": "Cappuccino",
     "description": "Espresso with steamed milk foam",
     "price": 4.99,
-    "image_url": "https://images.pexels.com/photos/533393/pexels-photo-533393.jpeg"
+    "image_url": "https://ichef.bbc.co.uk/ace/standard/1600/food/recipes/the_perfect_mocha_coffee_29100_16x9.jpg.webp"
   },
   {
     "id": 3,
@@ -31,7 +31,7 @@ export const testProducts: Product[] = [
     "name": "Mocha",
     "description": "Espresso with chocolate, steamed milk, and whipped cream",
     "price": 5.99,
-    "image_url": "https://images.pexels.com/photos/6331490/pexels-photo-6331490.jpeg"
+    "image_url": "https://gatherforbread.com/wp-content/uploads/2014/10/Dark-Chocolate-Mocha-Square.jpg"
   },
   {
     "id": 5,
@@ -47,7 +47,7 @@ export const testProducts: Product[] = [
     "name": "Iced Latte",
     "description": "Chilled espresso with milk and ice",
     "price": 5.49,
-    "image_url": "https://images.pexels.com/photos/3020917/pexels-photo-3020917.jpeg"
+    "image_url": "https://www.nescafe.com/br/sites/default/files/2023-08/RecipeHero_IcedCaramelLatte_1066x1066.jpg"
   },
   {
     "id": 7,
@@ -55,7 +55,7 @@ export const testProducts: Product[] = [
     "name": "Cold Brew",
     "description": "Slow-steeped coffee served cold and smooth",
     "price": 5.99,
-    "image_url": require("../images/cold-brew.jpg")
+    "image_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSow5TvR2THvfnjpgySH3yxE69iGpDAUQy14g&s"
   },
   {
     "id": 8,
@@ -63,7 +63,7 @@ export const testProducts: Product[] = [
     "name": "Pumpkin Spice Latte",
     "description": "Espresso with pumpkin spice, steamed milk, and whipped cream",
     "price": 6.49,
-    "image_url": "https://images.pexels.com/photos/4110005/pexels-photo-4110005.jpeg"
+    "image_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLBto1w9ZpFTnb5sv2x1ldYZqt-8FDxVglmg&s"
   },
   {
     "id": 9,
@@ -71,7 +71,7 @@ export const testProducts: Product[] = [
     "name": "Peppermint Mocha",
     "description": "Chocolatey espresso with peppermint, steamed milk, and whipped cream",
     "price": 6.49,
-    "image_url": "https://images.pexels.com/photos/302901/pexels-photo-302901.jpeg"
+    "image_url": "https://lifemadesweeter.com/wp-content/uploads/Peppermint-Latte-Photo-Recipe-Picture-3.jpg"
   },
   {
     "id": 10,
@@ -79,6 +79,6 @@ export const testProducts: Product[] = [
     "name": "Gingerbread Latte",
     "description": "Espresso with gingerbread syrup, steamed milk, and whipped cream",
     "price": 6.49,
-    "image_url": "https://images.pexels.com/photos/731932/pexels-photo-731932.jpeg"
+    "image_url": "https://flouringkitchen.com/wp-content/uploads/2023/10/gingerbread_latte_thumbnail.jpg"
   }
 ];
